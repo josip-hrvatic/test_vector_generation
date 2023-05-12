9c009b12671983b23584e291c8b401390d4553de
branimir1999 <branimir1999@gmail.com>
2023-05-12 15:06:22 +0200

    Added DB

--- my-app/src/server.js
+++ my-app/src/server.js
@@ -7,12 +7,12 @@ const fs = require('fs');
 // Create MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost', // Replace with your MySQL server hostname
-  user: 'root', // Replace with your MySQL username
-  password: 'password', // Replace with your MySQL password
-  database: 'mydatabase', // Replace with your MySQL database name
+  user: 'admin', // Replace with your MySQL username
+  password: 'placeholder', // Replace with your MySQL password
+  database: 'testvectorgeneration', // Replace with your MySQL database name
 });
 
-/*
+
 // Connect to MySQL
 connection.connect((err) => {
   if (err) {
@@ -21,14 +21,14 @@ connection.connect((err) => {
   }
   console.log('Connected to MySQL as id ' + connection.threadId);
 });
-*/
+
 
 // Create Express app
 const app = express();
 
 // Handle GET request
 app.get('/', (req, res) => {
-  connection.query('SELECT * FROM mytable', (err, rows) => {
+  connection.query('SELECT * FROM project', (err, rows) => {
     if (err) {
       console.error('Error executing MySQL query: ' + err.stack);
       res.status(500).send('Error executing MySQL query');
@@ -39,28 +39,61 @@ app.get('/', (req, res) => {
 });
 
 // Handle GET request
-app.get('/parse', (req, res) => {
-    let filePath = "./data/L2.json";
+app.get('/uploadDB', async function(req, res) {
+    let filePath = "./data/Demo.json";
 
-    let jsonObj;
+    let jsonData;
 
     try {
-        const jsonString = fs.readFileSync(filePath, 'utf8');
-        jsonObj = JSON.parse(jsonString);
+        jsonData = fs.readFileSync(filePath, 'utf8');
     } catch (error) {
-        console.error('Error parsing JSON string:', error);
-    }
-      
-      // Check if parsing was successful
-    if (jsonObj) {
-        console.log('Parsed JSON object:', jsonObj);
+        res.status(500).send('Error parsing JSON string:', error);
     }
 
-    res.json(jsonObj);
-});
-
-
+    try {
+      // Insert Project data
+      const project = JSON.parse(jsonData).Project;
+      const projectQuery = `INSERT INTO Project (Id, Name) VALUES (?, ?)`;
+      await executeQuery(projectQuery, [project.Id, project.Name]);
   
+      // Insert Sample data
+      const samples = JSON.parse(jsonData).Project.Samples;
+      const sampleQuery = `INSERT INTO Sample (Id, FamilyName, ProductName, Name) VALUES (?, ?, ?, ?)`;
+      await Promise.all(samples.map(sample => executeQuery(sampleQuery, [sample.Id, sample.FamilyName, sample.ProductName, sample.Name])));
+  
+      // Insert InputCondition data
+      const inputConditions = JSON.parse(jsonData).Project.InputConditions;
+      const inputConditionQuery = `INSERT INTO InputCondition (Id, Parameter, Min, Typical, Max, TimeBetweenPoints) VALUES (?, ?, ?, ?, ?, ?)`;
+      await Promise.all(inputConditions.map(inputCondition => executeQuery(inputConditionQuery, [inputCondition.Id, inputCondition.Parameter, inputCondition.Min, inputCondition.Typical, inputCondition.Max, inputCondition.TimeBetweenPoints])));
+  
+      // Insert TestPointCollections data
+      const testPointCollections = JSON.parse(jsonData).TestPointCollections;
+      const testPointCollectionQuery = `INSERT INTO TestPointCollections (Id, InputConditionId) VALUES (?, ?)`;
+      const testPointQuery = `INSERT INTO TestPoint (Id, Value, Unit) VALUES (?, ?, ?)`;
+  
+      for (const testPointCollection of testPointCollections) {
+        await executeQuery(testPointCollectionQuery, [testPointCollection.Id, testPointCollection.InputConditionId]);
+        const testPointCollectionId = testPointCollection.Id;
+  
+        for (const testPoint of testPointCollection.TestPoints) {
+          await executeQuery(testPointQuery, [testPoint.Id, testPoint.Value, testPoint.Unit]);
+          const testPointId = testPoint.Id;
+  
+          const testPointListQuery = `INSERT INTO TestPointList (TestPointId, TestPointCollectionId) VALUES (?, ?)`;
+          await executeQuery(testPointListQuery, [testPointId, testPointCollectionId]);
+        }
+  
+        for (const sampleId of testPointCollection.SampleIds) {
+          const sampleListQuery = `INSERT INTO SampleList (SampleId, TestPointCollectionId) VALUES (?, ?)`;
+          await executeQuery(sampleListQuery, [sampleId, testPointCollectionId]);
+        }
+      }
+  
+      console.log('Data inserted successfully!');
+    } catch (error) {
+      console.error('Error inserting data:', error);
+    }
+});
 
 
 // Handle POST request
@@ -75,4 +108,16 @@ app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
 
+async function executeQuery(query, params) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (error, results) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
 
