8cab1999ea589017c00ec5acd075e1abb33e79f5
Josip Hrvatic <josip.hrvatic@fer.hr>
2023-05-12 14:03:28 +0200

    InputConditions table init

--- my-app/src/InputConditions.js
+++ my-app/src/InputConditions.js
@@ -1,39 +1,36 @@
-import { List, Checkbox, Card } from 'antd';
-import {useState} from 'react';
-import TestVectorGenerator from "./TestVectorGenerator";
-// Define your data types based on your data structure
+import { Table } from 'antd';
+import React, { useState } from 'react';
 
-interface InputCondition {
-    Parameter: string;
-    Min: number;
-    Typical: number;
-    Max: number;
-    TimeBetweenPoints: number;
-    Id: number;
-}
-interface InputConditionsProps {
-    data: {
-        InputConditions: InputCondition[];
+const InputConditions = ({ data }) => {
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+    debugger;
+
+    const handleRowSelection = (selectedRowKeys) => {
+        setSelectedRowKeys(selectedRowKeys);
     };
-}
 
-const InputConditions: React.FC<InputConditionsProps> = ({ data }) => {
+    const columns = [
+        { title: 'Input Condition ID', dataIndex: 'Id', key: 'Id' },
+        { title: 'Parameter', dataIndex: 'Parameter', key: 'Parameter' },
+        { title: 'Minimum', dataIndex: 'Min', key: 'Min' },
+        { title: 'Maximum', dataIndex: 'Max', key: 'Max' },
+        { title: 'Time Between Points', dataIndex: 'TimeBetweenPoints', key: 'TimeBetweenPoints' },
+    ];
+
     return (
-        <List
-            itemLayout="vertical"
-            dataSource={data.InputConditions}
-            renderItem={(collection) => (
-                <List.Item>
-                    <Card title={`Test Point Collection ID: ${collection.Id}`}>
-                        <p>Parameter: {collection.Parameter}</p>
-                        <p>Minimum: {collection.Min}</p>
-                        <p>Typical Value: {collection.Typical}</p>
-                        <p>Maximum: {collection.Max}</p>
-                        <p>Time Between Points: {collection.TimeBetweenPoints}</p>
-                    </Card>
-                </List.Item>
-            )}
-        />
+        <>
+            <Table
+                columns={columns}
+                dataSource={data.InputConditions}
+                rowKey="Id"
+                rowSelection={{
+                    selectedRowKeys,
+                    onChange: handleRowSelection,
+                }}
+            />
+        </>
     );
 };
+
 export default InputConditions;
